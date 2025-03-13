ebb3bae4a241ac07454a6d9014b0abb4561db20b
alumo <aluno@aluno.com>
2025-03-12 21:47:28 -0300

    Adicionar arquivos de projeto.

---
+++ PrjVinculo/PrjVinculo/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace PrjVinculo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void btnProduto_Clicked(object sender, EventArgs e)
+        {
+            Produto produto = new Produto();
+
+            produto.Descricao = "Notebook Gamer";
+            produto.Preco = 5000;
+            produto.Categoria = "Informatica";
+
+            Navigation.PushAsync(new ProdutoPage() { BindingContext = produto});
+
+        }
+        private void btnProduto2_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewPage1());
+        }
+    }
+
+}
